fd144f3b0551d1855f79310777a0617315b03e0c
logicOnAbstractions <logiconabstractions@gmail.com>
2020-12-02 14:33:11 -0500

    Templates work

--- flask_on_docker/services/web/project/server.py
+++ flask_on_docker/services/web/project/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, send_from_directory, \
-    request, flash, redirect, url_for
+    request, flash, redirect, url_for, render_template
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 import os
@@ -35,39 +35,22 @@ def static_files(filename):
 def media_files(filename):
     return send_from_directory(app.config["MEDIA_FOLDER"], filename)
 
-@app.route("/upload", methods=["GET", "POST"])
-def upload_file():
-    if request.method == "POST":
-        file = request.files["file"]
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config["MEDIA_FOLDER"], filename))
-    return f"""
-    <!doctype html>
-    <title>upload new File</title>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file><input type=submit value=Upload>
-    </form>
-    """
-
-
 # a better approach from flask docs
-@app.route("/upload_2", methods=["GET", "POST"])
+@app.route("/upload", methods=["GET", "POST"])
 def upload_file_2():
+
+    # this actually is useless - because it's nginx who manages those
     if request.method == "POST":
         if "file" not in request.files:
             flash("no file part! select a file.")
-            return redirect(request.url)
+            # return redirect(request.url)
+            return "wrong call"
         file = request.files["file"]
         if file.filename == "":
             flash("empty file!")
-            return redirect(request.url)
+            # return redirect(request.url)
+            return "wrong call"
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config["MEDIA_FOLDER"], filename))
-
-        return f"""
-        <!doctype html>
-        <title>upload new File</title>
-        <form action="" method=post enctype=multipart/form-data>
-          <p><input type=file name=file><input type=submit value=Upload>
-        </form>
-        """
+    # flash("congrats! this worked")
+    return render_template("upload.html")
