d5fab714857db4e66b6d632c19356c317dc83ba8
logicOnAbstractions <logiconabstractions@gmail.com>
2020-12-02 09:26:17 -0500

    Fully working proj. Yeah. Going to implt some imprvmts i noted along the way now.

--- flask_on_docker/services/web/project/server.py
+++ flask_on_docker/services/web/project/server.py
@@ -1,5 +1,8 @@
-from flask import Flask, jsonify, send_from_directory
+from flask import Flask, jsonify, send_from_directory, \
+    request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 app.config.from_object("project.config.Config")     # configs - for now stuff that tells Flask where's the DB at
@@ -27,3 +30,44 @@ def hello_world():
 @app.route("/static/<path:filename>")
 def static_files(filename):
     return send_from_directory(app.config["STATIC_FOLDER"], filename)
+
+@app.route("/media/<path:filename>")
+def media_files(filename):
+    return send_from_directory(app.config["MEDIA_FOLDER"], filename)
+
+@app.route("/upload", methods=["GET", "POST"])
+def upload_file():
+    if request.method == "POST":
+        file = request.files["file"]
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config["MEDIA_FOLDER"], filename))
+    return f"""
+    <!doctype html>
+    <title>upload new File</title>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file><input type=submit value=Upload>
+    </form>
+    """
+
+
+# a better approach from flask docs
+@app.route("/upload_2", methods=["GET", "POST"])
+def upload_file_2():
+    if request.method == "POST":
+        if "file" not in request.files:
+            flash("no file part! select a file.")
+            return redirect(request.url)
+        file = request.files["file"]
+        if file.filename == "":
+            flash("empty file!")
+            return redirect(request.url)
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config["MEDIA_FOLDER"], filename))
+
+        return f"""
+        <!doctype html>
+        <title>upload new File</title>
+        <form action="" method=post enctype=multipart/form-data>
+          <p><input type=file name=file><input type=submit value=Upload>
+        </form>
+        """
